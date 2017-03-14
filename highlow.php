6d8c3264b71d762167a6cdfdfce73ac300a39f69
Matt Valdez <mattvaldez01@gmail.com>
2017-03-14 14:26:53 -0500

    made it to where the user can only use numbers to play the game

--- highlow.php
+++ highlow.php
@@ -3,7 +3,17 @@
 if ($argc == 3 && (is_numeric($argv[1]) && is_numeric($argv[2]))) {
 	$min = $argv[1];
 	$max = $argv[2];
-} else {	
+} elseif ($argc == 3 && (is_numeric($argv[1]) || is_numeric($argv[2]))) {	
+	fwrite(STDOUT, "Error: At least one of your inputs is not a numeric value" . PHP_EOL);	
+	do {
+		fwrite(STDOUT, "Please choose a min number: ");
+		$min = trim(fgets(STDIN));
+	} while (!is_numeric($min));
+	do {
+		fwrite(STDOUT, "Please choose a max number: ");
+		$max = trim(fgets(STDIN));
+	} while (!is_numeric($max));
+} else {
 	$min = 1;
 	$max = 100;
 }
@@ -12,19 +22,23 @@ $randomNumber = mt_rand($min, $max);
 $numberOfGuesses = 0;
 
 fwrite(STDOUT, "Guess a number between $min-$max: ");
-$answer = fgets(STDIN);
+$answer = trim(fgets(STDIN));
 
 while ($randomNumber !== $answer) {
-	if ($answer > $randomNumber) {
+	if (!is_numeric($answer)) {
+		$message = "Sorry, only numeric values are accepted. Please guess again: " . PHP_EOL;
+		fwrite(STDOUT, $message);
+		$answer = trim(fgets(STDIN));
+	} elseif ($answer > $randomNumber) {
 		$numberOfGuesses += 1;
 		$message = "LOWER... Guess Again: ";
 		fwrite(STDOUT, $message);
-		$answer = fgets(STDIN);
+		$answer = trim(fgets(STDIN));
 	} elseif ($answer < $randomNumber) {
 		$numberOfGuesses += 1;
 		$message = "HIGHER... Guess Again: ";
 		fwrite(STDOUT, $message);
-		$answer = fgets(STDIN);
+		$answer = trim(fgets(STDIN));
 	} else {
 		$numberOfGuesses += 1;
 		$message = "GOOD GUESS! It only took you $numberOfGuesses guesses!" . PHP_EOL;
